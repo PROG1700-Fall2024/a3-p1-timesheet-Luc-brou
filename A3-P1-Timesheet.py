3c9be2784200d3470a45cb4055848c8f8c98641e
Luc-brou <luc.r.brousseau@gmail.com>
2024-11-06 18:19:58 -0400

    VER 2 A3P1
    .

--- A3-P1-Timesheet.py
+++ A3-P1-Timesheet.py
@@ -3,8 +3,8 @@
 #               each of five work days from the user, then displays different 
 #               information calculated about those entries as output. 
 
-#Student #:     
-#Student Name:  
+#Student #w0489720:     
+#Student Name: Luc Brousseau 
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
@@ -14,10 +14,25 @@ def main():
        Earn=float(input(f"Enter the amounter earned for day {counter}: ")) #input field for daily earnings
        DailyEarn.append(Earn) #stores each days earnings 
     
-    print(DailyEarn) #prints stored values
-        
-        
 
+    max_day = DailyEarn.index(max(DailyEarn)) + 1 #is no day 0, so adds 1
+
+    print("-------------------------------------------------------------------------------------------------------") 
+    print (f"The highest amount earned per day was ${max(DailyEarn):.2f}0 dollars on day {max_day}.") #prints highest stored value, and the day associated with it
+    print("-------------------------------------------------------------------------------------------------------") 
+    
+    print (f"All together, you earned ${sum(DailyEarn)}0 dollars,") #adds all listed values together and prints them
+    print(f"With an average of ${(sum(DailyEarn))/5:.2f}0 dollars earned per day.") #adds all listed values together and divides them by the amount of days (5)
+    print("-------------------------------------------------------------------------------------------------------") 
+
+    print("You worked under 7 hours on:")
+    print("")
+    for counter in range(5):
+        if DailyEarn[counter] < 7:
+            print(f"Day {counter+1}: ${DailyEarn[counter]:.2f}")
+            found = True
+    print("-------------------------------------------------------------------------------------------------------") 
+    
 
 
 
