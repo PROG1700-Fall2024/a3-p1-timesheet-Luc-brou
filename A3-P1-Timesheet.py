c1c4911d416f94ba2697b52282e87822d5247aa9
Luc-brou <luc.r.brousseau@gmail.com>
2024-11-07 08:56:52 -0400

    Version 3
    .

--- A3-P1-Timesheet.py
+++ A3-P1-Timesheet.py
@@ -13,22 +13,26 @@ def main():
     for counter in range(1,6): #makes sure only 1-5 are printed for days
        Earn=float(input(f"Enter the amounter earned for day {counter}: ")) #input field for daily earnings
        DailyEarn.append(Earn) #stores each days earnings 
-    
+
+    for counter in range(5): #checks that number ISN'T negative, ends program if so
+        if DailyEarn[counter] < 0:
+            print("Please enter valid amounts (Cannot be negative).")
+            return
 
     max_day = DailyEarn.index(max(DailyEarn)) + 1 #is no day 0, so adds 1
 
     print("-------------------------------------------------------------------------------------------------------") 
-    print (f"The highest amount earned per day was ${max(DailyEarn):.2f}0 dollars on day {max_day}.") #prints highest stored value, and the day associated with it
+    print (f"The highest amount earned per day was ${max(DailyEarn):.2f} dollars on day {max_day}.") #prints highest stored value, and the day associated with it
     print("-------------------------------------------------------------------------------------------------------") 
     
-    print (f"All together, you earned ${sum(DailyEarn)}0 dollars,") #adds all listed values together and prints them
-    print(f"With an average of ${(sum(DailyEarn))/5:.2f}0 dollars earned per day.") #adds all listed values together and divides them by the amount of days (5)
+    print (f"All together, you earned ${sum(DailyEarn):.2f} dollars,") #adds all listed values together and prints them
+    print(f"With an average of ${(sum(DailyEarn))/5:.2f} dollars earned per day.") #adds all listed values together and divides them by the amount of days (5)
     print("-------------------------------------------------------------------------------------------------------") 
 
     print("You worked under 7 hours on:")
     print("")
-    for counter in range(5):
-        if DailyEarn[counter] < 7:
+    for counter in range(5): #prints value in a table list that are lower than 7 dollars
+        if DailyEarn[counter] <= 7:
             print(f"Day {counter+1}: ${DailyEarn[counter]:.2f}")
             found = True
     print("-------------------------------------------------------------------------------------------------------") 
